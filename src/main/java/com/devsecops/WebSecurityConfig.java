6d914099c74ed8ef724942775144c864bf6112dd
Ivan Escent <nomadius77@outlook.com>
2025-04-24 04:54:52 -0400

    rolling back

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -1,5 +1,15 @@
 package com.devsecops;
 
-public class WebSecurityConfig {
-    
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+    }
 }
